f8e2cf9e74ea0b94d37976bac30e4c770cf7cff5
ahmadyasser01 <47822365+ahmadyasser01@users.noreply.github.com>
2022-08-27 16:57:13 +0200

    Time: 6 ms (5.98%), Space: 6 MB (57.31%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
    
-    int dp[46];
+    // int dp[46];
     int climbStairs(int n) {
         
-        if(n<1) return 0;
-        if(dp[n]!=-0) return dp[n];
-        if(n==1) return dp[n] = 1;
-        if(n==2) return dp[n] = 2;
-        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
+        // if(n<1) return 0;
+        // if(dp[n]!=-0) return dp[n];
+        // if(n==1) return dp[n] = 1;
+        // if(n==2) return dp[n] = 2;
+        // return dp[n] = climbStairs(n-1) + climbStairs(n-2);
+        
+        int a=1,b=1,c;
+        for(int i=0;i<n-1;i++)
+        {
+            c =a ;
+            a = a+b;
+            b = c;
+        }
+        return a;
         
     }
 };
